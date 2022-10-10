a93351a82ecc5fb33788de3880fa04ca0dfcecbe
Vipin Negi <vipin@mindrops.com>
2022-10-10 15:51:22 +0530

    first commit

---
+++ new.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>CRC</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="bootstrap/css/style.css">
+        <script src="bootstrap/js/jquery.slim.min.js"></script>
+        <script src="bootstrap/js/popper.min.js"></script>
+        <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    </head>
+<body>
+<!-- <div class="container">
+    <div class="row">
+        <div class="col-sm-12 col-md-12">
+            <div class="img-bg">
+                    <img src="bootstrap/images/background.png" alt="">
+            </div>
+        </div>
+    </div>
+</div> -->
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 col-md-12">
+            <div class="page-title">
+                <a href="cff.php"><h1 class="lead">Climate change</h1></a>
+                <p class="sublead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis .</p>
+            </div>
+        </div>
+    </div>
+</div>
+<br><br><br>
+<div class="container-fluid">
+  <div class="row">
+    <div class="circle">                
+        <svg width="100%" height="550" viewBox="0 0 820 770">                   
+            <circle cx="420" cy="340" r="300" stroke="white" stroke-width="8" fill="#E0DEDE"></circle>
+        </svg>
+    </div>
+    <div class="lines">
+        <svg width="100%" height="100" viewBox="0 0 1720 220">
+            <circle cx="50" cy="80" r="40" stroke="white" stroke-width="4" fill="white" />
+        </svg>
+        <svg height="210" width="100%" viewBox="0 0 820 220">
+            <line x1="50" y1="0" x2="220" y2="100" style="stroke:rgb(255 255 255);stroke-width:4" />
+        </svg>
+    </div>
+      <div class="col-md-1 col-sm-12 text-right"></div>
+      <div class="col-md-2 col-sm-12 text-right">
+        <div class="text-box top-left">
+        <img src="bootstrap/images/2.png" alt="">
+          <h3><a href="#">Climate change</a> what is that</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
+        </div>
+        <div class="text-box middile-left">
+        <img src="bootstrap/images/2.png" alt="">
+          <h3><a href="#">what are the causes of</a> Climate change?</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
+        </div>
+        <div class="text-box bottom-left">
+        <img src="bootstrap/images/2.png" alt="">
+          <h3> Impact of climate</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+        </div>
+      </div>
+      <div class="col-md-6 col-sm-12 ">
+        <div class="choose-box">
+          <div class="inner-box">
+            <div class="content">
+                <!-- <img src="bootstrap/images/globe.gif" alt=""> -->
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-2 col-sm-12">
+        <div class="text-box top-right">
+        <img src="bootstrap/images/2.png" alt="">
+          <h3> climate change<a href="#"> mitigation and adaptation</a></h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+        </div>
+        <div class="text-box middile-right">
+        <img src="bootstrap/images/2.png" alt="">
+          <h3><a href="#">Climate change</a> trends for the Across the word</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
+        </div>
+        <div class="text-box bottom-right">
+        <img src="bootstrap/images/2.png" alt="">
+          <h3>Climate change<a href="#"> vulnerability</a></h3>
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
+        </div>
+      </div>
+      <div class="col-md-1 col-sm-12 text-right"></div>
+    </div>
+</div>
+
+</body>
+</html>
